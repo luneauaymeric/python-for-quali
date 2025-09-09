a6b575f98da1fadf7f8ba8f3e079d1beed73e110
Aymeric <aymeric.luneau@gmail.com>
2025-09-09 12:33:17 +0200

    requierment et sh script

---
+++ open-notebook.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This init script clones a Git repository that contains a Jupyter notebook
+# named `tutorial.ipynb` and opens it in Jupyter Lab at startup
+# Expected parameters : None
+
+# Clone repository and give permissions to the onyxia user
+GIT_REPO=python-for-quali
+git clone --depth 1 https://github.com/luneauaymeric/${GIT_REPO}.git
+chown -R onyxia:users ${GIT_REPO}/
+
+# Install additional packages if a requirements.txt file is present in the project
+REQUIREMENTS_FILE=${GIT_REPO}/requirements.txt
+[ -f $REQUIREMENTS_FILE ] && pip install -r $REQUIREMENTS_FILE
+
+# Open the relevant notebook when starting Jupyter Lab
+# echo "c.LabApp.default_url = '/lab/tree/${GIT_REPO}/bertopic_tutorial_git.ipynb'" >> /home/onyxia/.jupyter/jupyter_server_config.py
